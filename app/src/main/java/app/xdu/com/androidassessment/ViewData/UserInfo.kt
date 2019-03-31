bc9873e88915cfa649417584824f56d132450dc1
xdu <xdu@vmware.com>
2019-03-30 23:34:53 -0400

    #noissue -test app

---
+++ app/src/main/java/app/xdu/com/androidassessment/ViewData/UserInfo.kt
@@ -0,0 +1,6 @@
+package app.xdu.com.androidassessment.ViewData
+
+import android.graphics.Bitmap
+
+class UserInfo(var videoImage: Bitmap?, var profileImage: Bitmap?,
+               var videoTitle: String?, var numberOfViews: Int, var id: Int)
---
+++ app/src/main/java/app/xdu/com/androidassessment/data/VideoDetail.kt
@@ -0,0 +1,7 @@
+package app.xdu.com.androidassessment.data
+
+import app.xdu.com.androidassessment.ViewData.VideoDetailInfo
+
+
+class VideoDetail(var name: String?, var duration: String?, var number: Int,
+                  var imageUrl: String?, var link: String?, var videoDetailInfo: VideoDetailInfo?)
